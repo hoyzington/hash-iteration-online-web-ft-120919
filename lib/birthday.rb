a805b27d3eb3472ebc2ec2b59b4768ea0d67cc75
Scott Hoisington <hoyzington@gmail.com>
2019-12-12 20:36:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,13 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if birthday_kids[kids_name] > 12
+      puts ""
+    
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
